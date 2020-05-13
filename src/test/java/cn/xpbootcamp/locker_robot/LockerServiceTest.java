b4d5c9d08830421628cab0df4e82be2e536642ba
jiangze.mei <jiangze.mei@thoughtworks.com>
2020-05-13 18:57:42 +0800

    refactor : use static import making code clear

--- src/test/java/cn/xpbootcamp/locker_robot/LockerServiceTest.java
+++ src/test/java/cn/xpbootcamp/locker_robot/LockerServiceTest.java
@@ -1,9 +1,12 @@
 package cn.xpbootcamp.locker_robot;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 import cn.xpbootcamp.locker_robot.model.Bag;
 import cn.xpbootcamp.locker_robot.model.CommonConstant;
 import cn.xpbootcamp.locker_robot.model.ResultDto;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class LockerServiceTest {
@@ -13,8 +16,8 @@ class LockerServiceTest {
     Bag bag = new Bag();
     LockerService lockerService = new LockerService(3);
     ResultDto<Ticket> result = lockerService.store(bag);
-    Assertions.assertNotNull(result.getData());
-    Assertions.assertEquals(result.getMessage(), CommonConstant.STORE_SUCCESS_MESSAGE);
+    assertNotNull(result.getData());
+    assertEquals(result.getMessage(), CommonConstant.STORE_SUCCESS_MESSAGE);
   }
 
   @Test
@@ -22,8 +25,8 @@ class LockerServiceTest {
     Bag bag = new Bag();
     LockerService lockerService = new LockerService(0);
     ResultDto<Ticket> result = lockerService.store(bag);
-    Assertions.assertNull(result.getData());
-    Assertions.assertEquals(result.getMessage(), CommonConstant.STORE_FAILED_MESSAGE);
+    assertNull(result.getData());
+    assertEquals(result.getMessage(), CommonConstant.STORE_FAILED_MESSAGE);
   }
 
   @Test
@@ -33,7 +36,7 @@ class LockerServiceTest {
     ResultDto<Ticket> result = lockerService.store(bag);
 
     ResultDto<Bag> takeResult = lockerService.take(result.getData());
-    Assertions.assertNotNull(takeResult.getData());
-    Assertions.assertEquals(takeResult.getMessage(), CommonConstant.TAKE_SUCCESS_MESSAGE);
+    assertNotNull(takeResult.getData());
+    assertEquals(takeResult.getMessage(), CommonConstant.TAKE_SUCCESS_MESSAGE);
   }
 }
