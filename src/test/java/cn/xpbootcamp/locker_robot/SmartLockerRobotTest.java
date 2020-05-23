96499676fc3a487eb1836bcccf9e1117a8c571e5
Wei <wei.liu@thoughtworks.com>
2020-05-23 15:59:12 +0800

    [Liuwei|Jiangze] smart robot saving bag without available space

--- src/test/java/cn/xpbootcamp/locker_robot/SmartLockerRobotTest.java
+++ src/test/java/cn/xpbootcamp/locker_robot/SmartLockerRobotTest.java
@@ -7,9 +7,11 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
+import static cn.xpbootcamp.locker_robot.commom.CommonConstant.LOCKER_FULL;
 import static cn.xpbootcamp.locker_robot.commom.CommonConstant.STORE_SUCCESS_MESSAGE;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class SmartLockerRobotTest {
     @Test
@@ -30,5 +32,29 @@ public class SmartLockerRobotTest {
 
         assertNotNull(actualTicket);
         assertEquals(STORE_SUCCESS_MESSAGE, actualMessage);
+    }
+
+    @Test
+    void should_return_locker_is_full_message_when_store_bag_given_primary_locker_robot_and_no_space() {
+        // Given
+        Bag bag = new Bag();
+
+        Locker firstLocker = new Locker(1);
+        firstLocker.store(new Bag());
+        Locker secondLocker = new Locker(1);
+        secondLocker.store(new Bag());
+
+        SmartLockerRobot robot = new SmartLockerRobot();
+        robot.setOrderedLocker(Arrays.asList(firstLocker, secondLocker));
+
+        // When
+        ResultDto<Ticket> actualResult = robot.store(bag);
+
+        // Then
+        Ticket actualTicket = actualResult.getData();
+        String actualMessage = actualResult.getMessage();
+
+        assertNull(actualTicket);
+        assertEquals(LOCKER_FULL, actualMessage);
     }
 }
