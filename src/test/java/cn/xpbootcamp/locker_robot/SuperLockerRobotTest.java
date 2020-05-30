41595d8567d770cbea5fe468624a72a91c535f24
jiangze.mei <jiangze.mei@thoughtworks.com>
2020-05-30 16:41:29 +0800

    feat : store bag into the locker with max vacancy ratio

--- src/test/java/cn/xpbootcamp/locker_robot/SuperLockerRobotTest.java
+++ src/test/java/cn/xpbootcamp/locker_robot/SuperLockerRobotTest.java
@@ -58,4 +58,27 @@ public class SuperLockerRobotTest {
     assertNull(actualTicket);
     assertEquals(LOCKER_FULL, actualMessage);
   }
+
+  @Test
+  void should_store_in_locker_with_max_vacancy_ratio_and_return_ticket_when_store_bag_given_two_lockers_with_different_vacancy_ratio() {
+    // Given
+    Bag bag = new Bag();
+
+    Locker firstLocker = new Locker(5);
+    Locker secondLocker = new Locker(3);
+    SuperLockerRobot robot = new SuperLockerRobot();
+    robot.setOrderedLocker(Arrays.asList(firstLocker, secondLocker));
+
+    robot.store(new Bag());
+    robot.store(new Bag());
+
+    // When
+    ResultDto<Ticket> storeResult = robot.store(bag);
+
+    // Then
+    ResultDto<Bag> actualResult = firstLocker.take(storeResult.getData());
+    Bag actualBag = actualResult.getData();
+
+    assertEquals(bag, actualBag);
+  }
 }
